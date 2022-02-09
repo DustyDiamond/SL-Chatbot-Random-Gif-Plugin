614c4b4292c743a21e873186903a8b523ac87bc7
Dusty Diamond <julian@dustydiamond.de>
2022-02-09 22:22:02 +0100

    init

--- randomgif/randomgif_StreamlabsSystem.py
+++ randomgif/randomgif_StreamlabsSystem.py
@@ -1,10 +1,14 @@
 import os
 import json
 import codecs
+import random
+import time
+import PIL
+from shutil import copyfile
 
 from datetime import datetime
 
-ScriptName = "Radnom Gif Command"
+ScriptName = "Random Gif Command"
 Website = "http://www.dustydiamond.de/"
 Description = "Chooses a suitable gif for the game you're streaming on twitch"
 Creator = "DustyDiamond"
@@ -30,11 +34,14 @@ def Init():
     except:
         settings = {
             "command": "!gif",
+            "folder": "",
             "permission": "Everyone",
             "cooldown": 30,
             "bot_response": "Hier GIF einfuegen",
             "onCooldown": "$user, $command is still on cooldown for $cd seconds!",
-	        "onUserCooldown": "$user, $command is still on user cooldown for $cd seconds! "
+	        "onUserCooldown": "$user, $command is still on user cooldown for $cd seconds! ",
+            "delay": 2,
+            "gifsize": 600
         }
 
     return
@@ -42,38 +49,80 @@ def Init():
 def log(message):
     now = datetime.now()
     dt_string = now.strftime("%d.%m.%Y %H:%M:%S")
-    Parent.Log("INFO:", ScriptName + ": " + dt_string + ": " + message)
+    Parent.Log("INFO:" + dt_string + ": ", ScriptName + ": " + message)
     return
 
 def send_message(message):
     Parent.SendStreamMessage(message)
-    log("Message Sent")
+    #log("Message Sent")
     return
 
 def Execute(data):
     global game, GifDir
     message = ""
+    max = 0
+    number = 0
+    basewidth = 0
+
+    game = getGame()
+    game = game.replace(" ", "").lower()
+
     if data.IsChatMessage() and data.GetParam(0) == "!getgame" and Parent.HasPermission(data.User, "Moderator", ""):
-        game = getGame("cogefee")
-        game = replace(game, " ", "")
         message = "Current Game is: " + game
         
     if data.IsChatMessage() and data.GetParam(0) == settings["command"]:
-        game = getGame("cogefee")
-        game = game.replace(" ", "")
         location = GifDir
+        path = os.path.join(GifDir,game)
 
+        max = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path,name))])
+        if data.GetParam(1) != "":
+            try:
+                number = int(data.GetParam(1))
+                if number > max:
+                    number = random.randint(1,max)
+            except:
+                number = random.randint(1,max)
+        else:  
+            number = random.randint(1,max)
+
+        gif = str(number) + ".gif"
+
+        log("Max: " + str(max) + " - Rand: " + str(number))
+
+        delay = settings["delay"]
+        blank = "blank.gif"
+        blank_be = "blank-backup.gif"
+
+        # Display GIF, wait delay time
+        sourcefile = os.path.join(path,gif)
+        targetfile = os.path.join(GifDir,blank)
+
+        try: 
+            copyfile(sourcefile, targetfile)
+            log("Gif Copied")
+        except IOError as e:
+            log("Unable to Copy File. %s" % e)
+            return
+        
+        time.sleep(delay)
+
+        sourcefile = os.path.join(GifDir,blank_be)
+        targetfile = os.path.join(GifDir,blank)
+
+        try: 
+            copyfile(sourcefile, targetfile)
+            log("Gif Reset")
+        except IOError as e:
+            log("Unable to Copy File. %s" % e)
+            return
         
     send_message(message)
     return
 
 
-def getGame(channel):
-    global game, jsonData
-    if channel == "":
-        channel = Parent.GetChannelName()
-    
-    jsonData = json.loads(Parent.GetRequest("https://decapi.me/twitch/game/" + channel, {}))
+def getGame():
+    global game, jsonData    
+    jsonData = json.loads(Parent.GetRequest("https://decapi.me/twitch/game/" + Parent.GetChannelName(), {}))
     game = jsonData["response"]
     return game
 
