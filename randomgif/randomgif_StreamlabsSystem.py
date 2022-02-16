19ad12bb75e379c1bf9db672b4531829f8233e25
Dusty Diamond <julian@dustydiamond.de>
2022-02-17 00:55:16 +0100

    v1.0.2 release

--- randomgif/randomgif_StreamlabsSystem.py
+++ randomgif/randomgif_StreamlabsSystem.py
@@ -32,6 +32,7 @@ Command = "!gif"
 # SLOBS RC
 BridgeApp = os.path.join(os.path.dirname(__file__), "bridge\\SLOBSRC.exe")
 RegObsScene = None
+RegObsGetItems = None
 RegObsSource = None
 RegObsSourceT = None
 RegObsFolder = None
@@ -53,6 +54,7 @@ def Init():
     # Init for SLOBS RC
     # Globals
     global RegObsScene
+    global RegObsGetItems
     global RegObsSource
     global RegObsSourceT
     global RegObsFolder
@@ -61,6 +63,7 @@ def Init():
     global RegObsReplaySwap
     # Compile regexes in init
     RegObsScene = re.compile(r"(?:\$SLOBSscene\([\ ]*[\"\'](?P<scene>[^\"\']+)[\"\'][\ ]*(?:\,[\ ]*[\"\'](?P<delay>\d*)[\"\'][\ ]*)?\))", re.U)
+    RegObsGetItems = re.compile(r"(?:\$SLOBSgetItems\([\ ]*[\"\'](?P<scene>[^\"\']+)[\"\'][\ ]*)?\)", re.U)
     RegObsSource = re.compile(r"(?:\$SLOBSsource\([\ ]*[\"\'](?P<source>[^\"\']+)[\"\'][\ ]*\,[\ ]*[\"\'](?P<visibility>[^\"\']*)[\"\'][\ ]*(?:\,[\ ]*[\"\'](?P<scene>[^\"\']*)[\"\'][\ ]*)?\))", re.U)
     RegObsSourceT = re.compile(r"(?:\$SLOBSsourceT\([\ ]*[\"\'](?P<source>[^\"\']+)[\"\'][\ ]*\,[\ ]*[\"\'](?P<mode>[^\"\']*)[\"\'][\ ]*\,[\ ]*[\"\'](?P<delay>\d+)[\"\'][\ ]*(?:\,[\ ]*[\"\'](?P<scene>[^\"\']*)[\"\'][\ ]*)?\))", re.U)
     RegObsFolder = re.compile(r"(?:\$SLOBSfolder\([\ ]*[\"\'](?P<folder>[^\"\']+)[\"\'][\ ]*\,[\ ]*[\"\'](?P<visibility>[^\"\']*)[\"\'][\ ]*(?:\,[\ ]*[\"\'](?P<scene>[^\"\']*)[\"\'][\ ]*)?\))", re.U)
@@ -111,44 +114,6 @@ def send_message(message):
     return
 
 #---------------------------------------------------------
-# send json-rpc to SLOBS
-#---------------------------------------------------------
-def getobsscenes():
-    
-    return request(1,'ScenesService','getScenes')
-
-def getsceneitems(id = "1aaa222b3333"):
-    
-    return request(2, id,'getItems')
-
-#---------------------------------------------------------
-# Helper for SLOBS API
-#---------------------------------------------------------
-def request(id, resourceId, methodName):
-    url = settings["apiaddress"]
-    port = settings["apiport"]
-    url = url + ":" + port + "/api"
-    token = "Bearer " + settings["token"]
-    
-    # Get Scenes
-    if id == 1:
-        payload = "{'jsonrpc': '2.0', 'id': " + str(id) + ", 'method': " + str(methodName) + ",'params': {'resource': " + str(resourceId) + "}}"
-    
-    # Get Sources in given Scene
-    elif id == 2:
-        resourceId = "Scene[\"" + resourceId + "\"]"
-        payload = "{'jsonrpc': '2.0', 'id': " + str(id) + ", 'method': " + str(methodName) + ", 'params': {'resource': " + str(resourceId) + ", 'args': []}}"
-    
-    # Pipe handling using subprocesses
-    # \\.\pipe\slobs
-    
-       
-    #response = Parent.PostRequest(url, headers, payload, True)
-    #response = Parent.GetRequest(url, headers)
-
-    return 
-
-#---------------------------------------------------------
 # execute event, called when a message is posted in to 
 # twitch chat. data contains info about message and user etc
 #---------------------------------------------------------
@@ -160,26 +125,37 @@ def Execute(data):
 
     game = getGame()
     game = game.replace(" ", "").lower()
+    scene = settings["scene"]
+    scene_list = []
 
     # !getgame command to put game as interpreted in script in the chat for naming purposes
     if data.IsChatMessage() and data.GetParam(0) == "!getgame" and Parent.HasPermission(data.User, "Moderator", ""):
         message = "Current Game is: " + game
 
     # !getscenes 
-    if data.IsChatMessage() and data.GetParam(0) == "!getscenes" and Parent.HasPermission(data.User, "Moderator", ""):
-        result = getobsscenes()
+    #if data.IsChatMessage() and data.GetParam(0) == "!getscenes" and Parent.HasPermission(data.User, "Moderator", ""):
+        #result = getobsscenes()
         #log(result)
         
     # !getsources
-    if data.IsChatMessage() and data.GetParam(0) == "!getsources" and Parent.HasPermission(data.User, "Moderator", ""):
-        result = getsceneitems()
+    #if data.IsChatMessage() and data.GetParam(0) == "!getsources" and Parent.HasPermission(data.User, "Moderator", ""):
+        #result = getsceneitems()
         #log(result)
 
     # main branch 
     if data.IsChatMessage() and data.GetParam(0) == settings["command"]:
         # get Max from Nr of Sources in Scene
-        # max = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path,name))])
-        max = 4
+        bridge_answer = GetItems(scene)
+        log(bridge_answer)
+        max = 1
+
+        temp = bridge_answer
+        log(temp)
+        temp = left(temp, len(temp)-1)
+        temp = right(temp, len(temp)-8)
+        scene_list = temp.split(",")
+        max = scene_list.count(game)
+        log(str(max))
         # get random number to determine
         if data.GetParam(1) != "":
             try:
@@ -193,11 +169,11 @@ def Execute(data):
 
         gif = game + "-" + str(number)
 
-        #log("Max: " + str(max) + " - Rand: " + str(number))
+        log("Max: " + str(max) + " - Rand: " + str(number))
 
         delay = str(settings["delay"])
         #message = '$SLOBSsourceT("' + gif + '", "onoff", "' + delay + '", "gifs")'
-        SetSourceVisibilityTimed(gif,"onoff",delay,"gifs")
+        SetSourceVisibilityTimed(gif,"onoff",delay,scene)
         
     send_message(message)
     return
@@ -240,6 +216,10 @@ def mid(s, offset, amount):
 #---------------------------------------------------------
 # SLOBS RC Functions
 #---------------------------------------------------------
+def GetItems(scene):
+    """ Get Items contained in given Scene """
+    return os.popen("{0} get_items \"{1}\"".format(BridgeApp,scene)).read()
+
 def ChangeScene(scene, delay=None):
 	""" Change to scene. """
 	if delay:
@@ -305,201 +285,212 @@ def ThreadedFunction(command):
 # Parse parameters
 #---------------------------------------
 def Parse(parseString, user, target, message):
-	""" Custom Parameter Parser. """
+    """ Custom Parameter Parser. """
+
+    # $SLOBSgetItems("scene")
+    if "$SLOBSgetItems" in parseString:
+        result = RegObsGetItems.search(parseString)
+        if result:
+            fullParameterMatch = result.group(0)
+            scene = result.group("scene")
+
+            threading.Thread(target=GetItems, args=(scene)).start()
+
+            return parseString
 
 	# $SLOBSscene("scene")
 	# $SLOBSscene("scene", "delay")
-	if "$SLOBSscene" in parseString:
+    if "$SLOBSscene" in parseString:
 		
-		# Apply regex to verify correct parameter use
-		result = RegObsScene.search(parseString)
-		if result:		
+        # Apply regex to verify correct parameter use
+        result = RegObsScene.search(parseString)
+        if result:		
 			
 			# Get results from regex match
-			fullParameterMatch = result.group(0)
-			scene = result.group("scene")
-			delay = result.group("delay")
+            fullParameterMatch = result.group(0)
+            scene = result.group("scene")
+            delay = result.group("delay")
 
 			# Start ChangeScene in separate thread
-			threading.Thread(target=ChangeScene, args=(scene, delay)).start()
+            threading.Thread(target=ChangeScene, args=(scene, delay)).start()
 
 			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+            return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSswap("scene", "delay")
 	# $SLOBSswap("scene", "delay", "returnscene")
-	if "$SLOBSswap" in parseString:
+    if "$SLOBSswap" in parseString:
 	
 		# Apply regex to verify correct parameter use
-		result = RegObsSwap.search(parseString)
-		if result:
+        result = RegObsSwap.search(parseString)
+        if result:
 
 			# Get results from regex match
-			fullParameterMatch = result.group(0)
-			scene = result.group("scene")
-			delay = result.group("delay")
-			returnscene = result.group("returnscene")
+            fullParameterMatch = result.group(0)
+            scene = result.group("scene")
+            delay = result.group("delay")
+            returnscene = result.group("returnscene")
 
 			# Start ChangeSceneTimed in separate thread
-			threading.Thread(target=ChangeSceneTimed, args=(scene, delay, returnscene)).start()
+            threading.Thread(target=ChangeSceneTimed, args=(scene, delay, returnscene)).start()
 
 			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+        return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSsourceT("source", "mode", "delay")
 	# $SLOBSsourceT("source", "mode", "delay", "scene")
-	if "$SLOBSsourceT" in parseString:
+    if "$SLOBSsourceT" in parseString:
 
 		# Apply regex to verify correct parameter use
-		result = RegObsSourceT.search(parseString)
-		if result:
+        result = RegObsSourceT.search(parseString)
+        if result:
 
 			# Get match groups from regex
-			fullParameterMatch = result.group(0)
-			source = result.group("source")
-			mode = result.group("mode")
-			delay = result.group("delay")
-			scene = result.group("scene")
+            fullParameterMatch = result.group(0)
+            source = result.group("source")
+            mode = result.group("mode")
+            delay = result.group("delay")
+            scene = result.group("scene")
 
 			# Start SetSourceVisibilityTimed in separate thread
-			threading.Thread(target=SetSourceVisibilityTimed, args=(source, mode, delay, scene)).start()
+            threading.Thread(target=SetSourceVisibilityTimed, args=(source, mode, delay, scene)).start()
 
 			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+            return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSsource("source", "visibility")
 	# $SLOBSsource("source", "visibility", "scene")
-	if "$SLOBSsource" in parseString:
+    if "$SLOBSsource" in parseString:
 
 		# Apply regex to verify correct parameter use
-		result = RegObsSource.search(parseString)
-		if result:
+        result = RegObsSource.search(parseString)
+        if result:
 
-			# Get match groups from regex
-			fullParameterMatch = result.group(0)
-			source = result.group("source")
-			visibility = result.group("visibility")
-			scene = result.group("scene")
+            # Get match groups from regex
+            fullParameterMatch = result.group(0)
+            source = result.group("source")
+            visibility = result.group("visibility")
+            scene = result.group("scene")
 
-			# Start SetSourceVisibility in separate thread
-			threading.Thread(target=SetSourceVisibility, args=(source, visibility, scene)).start()
+            # Start SetSourceVisibility in separate thread
+            threading.Thread(target=SetSourceVisibility, args=(source, visibility, scene)).start()
 
-			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+            # Replace the whole parameter with an empty string
+            return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSfolderT("folder", "mode", "delay")
 	# $SLOBSfolderT("folder", "mode", "delay", "scene")
-	if "$SLOBSfolderT" in parseString:
+    if "$SLOBSfolderT" in parseString:
 
 		# Apply regex to verify correct parameter use
-		result = RegObsFolderT.search(parseString)
-		if result:
+        result = RegObsFolderT.search(parseString)
+        if result:
 
-			# Get match groups from regex
-			fullParameterMatch = result.group(0)
-			folder = result.group("folder")
-			mode = result.group("mode")
-			delay = result.group("delay")
-			scene = result.group("scene")
+            # Get match groups from regex
+            fullParameterMatch = result.group(0)
+            folder = result.group("folder")
+            mode = result.group("mode")
+            delay = result.group("delay")
+            scene = result.group("scene")
 
-			# Start SetFolderVisibilityTimed in separate thread
-			threading.Thread(target=SetFolderVisibilityTimed, args=(folder, mode, delay, scene)).start()
+            # Start SetFolderVisibilityTimed in separate thread
+            threading.Thread(target=SetFolderVisibilityTimed, args=(folder, mode, delay, scene)).start()
 
-			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+            # Replace the whole parameter with an empty string
+            return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSfolder("folder", "visibility")
 	# $SLOBSfolder("folder", "visibility", "scene")
-	if "$SLOBSfolder" in parseString:
+    if "$SLOBSfolder" in parseString:
 
 		# Apply regex to verify correct parameter use
-		result = RegObsFolder.search(parseString)
-		if result:
-			
-			# Get match groups from regex
-			fullParameterMatch = result.group(0)
-			folder = result.group("folder")
-			visibility = result.group("visibility")
-			scene = result.group("scene")
+        result = RegObsFolder.search(parseString)
+        if result:
+            
+            # Get match groups from regex
+            fullParameterMatch = result.group(0)
+            folder = result.group("folder")
+            visibility = result.group("visibility")
+            scene = result.group("scene")
 
-			# Start SetFolderVisibility in separate thread
-			threading.Thread(target=SetFolderVisibility, args=(folder, visibility, scene)).start()
+            # Start SetFolderVisibility in separate thread
+            threading.Thread(target=SetFolderVisibility, args=(folder, visibility, scene)).start()
 
-			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+            # Replace the whole parameter with an empty string
+            return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSstartRecording
-	if "$SLOBSstartRecording" in parseString:
+    if "$SLOBSstartRecording" in parseString:
 
-		# Start Start Recording in separate thread
-		threading.Thread(target=ThreadedFunction, args=("start_recording",)).start()
-    
-		# Replace $SLOBSstop with empty string
-		return parseString.replace("$SLOBSstartRecording", "")
+        # Start Start Recording in separate thread
+        threading.Thread(target=ThreadedFunction, args=("start_recording",)).start()
+
+        # Replace $SLOBSstop with empty string
+        return parseString.replace("$SLOBSstartRecording", "")
 	
-	# $SLOBSstopRecording
-	if "$SLOBSstopRecording" in parseString:
+    # $SLOBSstopRecording
+    if "$SLOBSstopRecording" in parseString:
 
-		# Start Stop Recording in separate thread
-		threading.Thread(target=ThreadedFunction, args=("stop_recording",)).start()
-    
-		# Replace $SLOBSstop with empty string
-		return parseString.replace("$SLOBSstopRecording", "")
+        # Start Stop Recording in separate thread
+        threading.Thread(target=ThreadedFunction, args=("stop_recording",)).start()
+
+        # Replace $SLOBSstop with empty string
+        return parseString.replace("$SLOBSstopRecording", "")
 	
 	# $SLOBSstartReplay
-	if "$SLOBSstartReplay" in parseString:
+    if "$SLOBSstartReplay" in parseString:
 
-		# Start Start Replay Buffer in separate thread
-		threading.Thread(target=ThreadedFunction, args=("start_replaybuffer",)).start()
-    
-		# Replace $SLOBSstop with empty string
-		return parseString.replace("$SLOBSstartReplay", "")
+        # Start Start Replay Buffer in separate thread
+        threading.Thread(target=ThreadedFunction, args=("start_replaybuffer",)).start()
 
-	# $SLOBSstopReplay
-	if "$SLOBSstopReplay" in parseString:
+        # Replace $SLOBSstop with empty string
+        return parseString.replace("$SLOBSstartReplay", "")
 
-    	# Start Sttop Replay Buffer in separate thread
-		threading.Thread(target=ThreadedFunction, args=("stop_replaybuffer",)).start()
+    # $SLOBSstopReplay
+    if "$SLOBSstopReplay" in parseString:
 
-		# Replace $SLOBSstop with empty string
-		return parseString.replace("$SLOBSstopReplay", "")
+        # Start Sttop Replay Buffer in separate thread
+        threading.Thread(target=ThreadedFunction, args=("stop_replaybuffer",)).start()
+
+        # Replace $SLOBSstop with empty string
+        return parseString.replace("$SLOBSstopReplay", "")
 
 	# $SLOBSsaveReplaySwap("scene")
 	# $SLOBSsaveReplaySwap("scene","offset")
-	if "$SLOBSsaveReplaySwap" in parseString:
+    if "$SLOBSsaveReplaySwap" in parseString:
 
-		# Apply regex to verify correct parameter use
-		result = RegObsReplaySwap.search(parseString)
-		if result:		
+        # Apply regex to verify correct parameter use
+        result = RegObsReplaySwap.search(parseString)
+        if result:		
 			
-			# Get results from regex match
-			fullParameterMatch = result.group(0)
-			scene = result.group("scene")
-			offset = result.group("offset")
+            # Get results from regex match
+            fullParameterMatch = result.group(0)
+            scene = result.group("scene")
+            offset = result.group("offset")
 
-			# Start Save Replay and Swap in separate thread
-			threading.Thread(target=SaveReplaySwap, args=(scene, offset)).start()
+            # Start Save Replay and Swap in separate thread
+            threading.Thread(target=SaveReplaySwap, args=(scene, offset)).start()
 
-			# Replace the whole parameter with an empty string
-			return parseString.replace(fullParameterMatch, "")
+            # Replace the whole parameter with an empty string
+            return parseString.replace(fullParameterMatch, "")
 
 	# $SLOBSsaveReplay
-	if "$SLOBSsaveReplay" in parseString:
+    if "$SLOBSsaveReplay" in parseString:
 
-		# Start Save Replay in separate thread
-		threading.Thread(target=ThreadedFunction, args=("save_replaybuffer",)).start()
-    
-		# Replace $SLOBSstop with empty string
-		return parseString.replace("$SLOBSsaveReplay", "")
+        # Start Save Replay in separate thread
+        threading.Thread(target=ThreadedFunction, args=("save_replaybuffer",)).start()
+
+        # Replace $SLOBSstop with empty string
+        return parseString.replace("$SLOBSsaveReplay", "")
 
 	# $SLOBSstopStreaming
-	if "$SLOBSstopStreaming" in parseString:
+    if "$SLOBSstopStreaming" in parseString:
     
-		# Start stop streaming in separate thread
-		threading.Thread(target=ThreadedFunction, args=("stop_streaming",)).start()
-    
-		# Replace $SLOBSstop with empty string
-		return parseString.replace("$SLOBSstop", "")
+        # Start stop streaming in separate thread
+        threading.Thread(target=ThreadedFunction, args=("stop_streaming",)).start()
+
+        # Replace $SLOBSstop with empty string
+        return parseString.replace("$SLOBSstop", "")
 
 	# $SLOBSstartStreaming
 #	if "$SLOBSstartStreaming" in parseString:
@@ -512,4 +503,4 @@ def Parse(parseString, user, target, message):
 		    
 		    
 	# Return unaltered parseString
-	return parseString
+    return parseString
